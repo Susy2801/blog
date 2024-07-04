27616bae770596ac9953e83a60e5a71c8fe1cc40
Susy2801 <nduy200@gmail.com>
2024-07-04 10:12:22 +0700

    vercel config

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://susu0208:duy983868@cluster0.r4nm2hl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Kết nối cơ sở dữ liệu thành công!");
   } catch (error) {
     console.log(`Lỗi:` + error);
